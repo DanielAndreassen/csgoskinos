205665a377a2065a3192bab03a93ef00f7317c7a
Daniel <andreassen.daniel@elev.ga.ntig.se>
2022-04-13 16:19:22 +0200

    Javascript start

---
+++ main.js
@@ -0,0 +1,13 @@
+const btn1 = document.querySelector(".bottombutton1");
+
+btn1.addEventListener("click", (e) =>{
+    let gun1 = document.querySelector(".gunstats");
+    console.log(gun1);
+});
+
+const btn2 = document.querySelector(".bottombutton2");
+
+btn2.addEventListener("click", (e) => {
+    let gun2 = document.querySelector(".gunstats")
+    console.log(gun2);
+});
