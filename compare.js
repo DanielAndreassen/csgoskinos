e10339823904627c610fdbbc5fd915133d201385
Daniel <andreassen.daniel@elev.ga.ntig.se>
2022-04-14 16:14:39 +0200

    more trying to figure out java

---
+++ compare.js
@@ -0,0 +1,3 @@
+JSON.parse(window.localStorage.getItem('gun_1'));
+
+const compare1 = document.querySelector(".maincontainer1");
--- main.js
+++ main.js
@@ -1,13 +1,21 @@
+let gun1;
+let gun2;
+
 const btn1 = document.querySelector(".bottombutton1");
-
-btn1.addEventListener("click", (e) =>{
-    let gun1 = document.querySelector(".gunstats");
-    console.log(gun1);
-});
-
 const btn2 = document.querySelector(".bottombutton2");
 
-btn2.addEventListener("click", (e) => {
-    let gun2 = document.querySelector(".gunstats")
-    console.log(gun2);
+btn1.addEventListener("click", (e) => {
+  gun1 = document.querySelectorAll(".maincontainer");
+  console.log(gun1);
+  return gun1;
 });
+
+btn2.addEventListener("click", (e) => {
+  gun2 = document.querySelectorAll(".maincontainer");
+  console.log(gun2);
+  return gun2;
+});
+
+localStorage.setItem('gun_1', JSON.stringify(gun1));
+
+localStorage.setItem('gun_1', JSON.stringify(gun2));
